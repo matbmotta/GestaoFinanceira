40fdc1f0a6f48bb0e64395e89efd0470bf7cc4b3
matbmotta <mat-motta@hotmail.com>
2022-09-26 21:35:32 -0300

    Alteração da parte de preço e valor frete

--- src/br/com/up/GestaoFinanceira/CompraInternacional.java
+++ src/br/com/up/GestaoFinanceira/CompraInternacional.java
@@ -5,9 +5,10 @@ import br.com.up.GestaoFinanceira.Compra;
 public class CompraInternacional extends Compra {
 
     public void CalculaValorFornecedor() {
-        double valorTotalCompra = this.Preco * this.Quantidade;
+        this.Preco  = this.produto.Valor * this.Quantidade;
 
-        System.out.println("O valor da compra é: " + valorTotalCompra);
-        System.out.println("O valor do estoque é: " + valorTotalCompra);
+
+        System.out.println("O valor da compra é: " + Preco);
+        System.out.println("O valor do estoque é: " + Preco);
     }
 }
--- src/br/com/up/GestaoFinanceira/CompraNacional.java
+++ src/br/com/up/GestaoFinanceira/CompraNacional.java
@@ -3,17 +3,16 @@ package br.com.up.GestaoFinanceira;
 import br.com.up.GestaoFinanceira.Compra;
 
 public class CompraNacional extends Compra {
-    public double ValorFrete;
 
     public void CalculaValorFornecedor(){
-        this.ValorFrete = 50;
-        double valorTotalCompra = this.Preco * this.Quantidade;
-        double icms = valorTotalCompra * 0.18;
-        double iss = ValorFrete * 0.05;
+        double valorFrete = 50;
+        this.Preco = this.produto.Valor * this.Quantidade;
+        double icms = Preco * 0.18;
+        double iss = valorFrete * 0.05;
 
-        System.out.println("O valor da compra é: " + valorTotalCompra);
-        System.out.println("O valor do estoque é: " + valorTotalCompra);
-        System.out.println("O valor da compra com o frete é:" + valorTotalCompra + ValorFrete);
+        System.out.println("O valor da compra é: " + Preco);
+        System.out.println("O valor do estoque é: " + Preco);
+        System.out.println("O valor da compra com o frete é:" + Preco + valorFrete);
         System.out.println("O valor dos impostos ICMS e ISS é:" + icms + iss);
     };
 }
